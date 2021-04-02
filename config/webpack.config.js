5d94290f43d33b443920f346ec66ccb9d4fc2b87
BudiSetyo <budisetyo176@gmail.com>
2021-04-02 09:54:22 +0700

    settings

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -501,15 +501,25 @@ module.exports = function (webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
+              use: [
+                ...getStyleLoaders(
+                  {
+                    importLoaders: 3,
+                    sourceMap: isEnvProduction
+                      ? shouldUseSourceMap
+                      : isEnvDevelopment,
+                  },
+                  'sass-loader'
+                ),
                 {
-                  importLoaders: 3,
-                  sourceMap: isEnvProduction
-                    ? shouldUseSourceMap
-                    : isEnvDevelopment,
+                  loader: 'sass-resources-loader',
+                  options: {
+                    resources: [
+                      path.join(process.cwd(), 'src/assets/scss/main.scss'),
+                    ],
+                  },
                 },
-                'sass-loader',
-              ),
+              ],
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
@@ -520,18 +530,28 @@ module.exports = function (webpackEnv) {
             // using the extension .module.scss or .module.sass
             {
               test: sassModuleRegex,
-              use: getStyleLoaders(
+              use: [
+                ...getStyleLoaders(
+                  {
+                    importLoaders: 3,
+                    sourceMap: isEnvProduction
+                      ? shouldUseSourceMap
+                      : isEnvDevelopment,
+                    modules: {
+                      getLocalIdent: getCSSModuleLocalIdent,
+                    },
+                  },
+                  'sass-loader'
+                ),
                 {
-                  importLoaders: 3,
-                  sourceMap: isEnvProduction
-                    ? shouldUseSourceMap
-                    : isEnvDevelopment,
-                  modules: {
-                    getLocalIdent: getCSSModuleLocalIdent,
+                  loader: 'sass-resources-loader',
+                  options: {
+                    resources: [
+                      path.join(process.cwd(), 'src/assets/scss/main.scss'),
+                    ],
                   },
                 },
-                'sass-loader',
-              ),
+              ],
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Button from '@/components/Button/index';
-import './App.css';
+import Button from '@/components/button/index';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Button />
+      <h1 className="text--red">Bambang Setyo</h1>
     </div>
   );
 }
